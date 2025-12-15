06b2ac6379890437198479fa91288f4151dfafca
Atto9752 <anto.faundes@duocuc.cl>
2025-12-15 00:22:37 -0300

    correcciones importantes
    correcciones para que se puedan conectar al S3 con el front

--- fs2-back-productos-main/fs2-back-productos-main/productos/src/main/java/com/example/stroms/productos/config/CorsConfig.java
+++ fs2-back-productos-main/fs2-back-productos-main/productos/src/main/java/com/example/stroms/productos/config/CorsConfig.java
@@ -1,30 +1,21 @@
-package com.example.carrito.config;
+package com.example.stroms.productos.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig implements WebMvcConfigurer{
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
+    
         final String S3_FRONTEND_URL = "http://bucket-app-stroms-solutions-s3.s3-website-us-east-1.amazonaws.com/";
-
-        registry.addMapping("/**") // aplica a todos los endpoints
-            .allowedOrigins(S3_FRONTEND_URL, "http://localhost:3000") // permite S3 y localhost
+        
+        registry.addMapping("/**") 
+            .allowedOrigins(S3_FRONTEND_URL, "http://localhost:3000") 
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
             .allowedHeaders("*")
             .allowCredentials(true);
     }
-
 }
